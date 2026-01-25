40aafcec83114c47181f90f9467ccc8ceeeb45fe
Marlene Mariana De La Cruz Padilla <mardlc717@outlook.com>
2026-01-25 14:39:44 -0600

    Practica 7 Audio
    Audio y control de Audio

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance { get; private set; }
-
     private GameManager gameManager;
 
     // ==============================
@@ -35,6 +35,14 @@ public class UIManager : MonoBehaviour
     public Image vida3Cheedor;
     private bool isPaused = false;
 
+    // ==============================
+    // AUDIO
+    // ==============================
+    public TextMeshProUGUI musicLevelText;
+    public TextMeshProUGUI sfxLevelText;
+    private int musicLevel = 5;
+    private int sfxLevel = 5;
+
     private ConfigSource configSource;
 
     public enum ConfigSource
@@ -46,10 +54,8 @@ public class UIManager : MonoBehaviour
 
     private void Awake()
     {
-        if (Instance == null)
-            Instance = this;
-        else
-            Destroy(gameObject);
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
     }
 
     private void Start()
@@ -72,6 +78,11 @@ public class UIManager : MonoBehaviour
         gameManager = FindFirstObjectByType<GameManager>();
         gameManager.PlayMusicByState(GameManager.GameState.Menu);
 
+        // Inicializar textos de audio
+        if (musicLevelText != null) musicLevelText.text = musicLevel.ToString();
+        if (sfxLevelText != null) sfxLevelText.text = sfxLevel.ToString();
+
+
         Time.timeScale = 0f; 
     }
 
@@ -82,7 +93,6 @@ public class UIManager : MonoBehaviour
             Keyboard.current != null &&
             Keyboard.current.enterKey.wasPressedThisFrame)
         {
-            Debug.Log("Enter detectado");
             GoToMenuPrincipal();
         }
     }
@@ -230,6 +240,37 @@ public class UIManager : MonoBehaviour
         MenuPrincipal.SetActive(true);
         gameManager.PlayMusicByState(GameManager.GameState.Menu);
         Salir();
+    }
+
+    // =========AUDIO=========
+
+    public void IncreaseMusic() => ChangeMusicVolume(1);
+    public void DecreaseMusic() => ChangeMusicVolume(-1);
+    public void IncreaseSFX() => ChangeSFXVolume(1);
+    public void DecreaseSFX() => ChangeSFXVolume(-1);
+
+    private void ChangeMusicVolume(int amount)
+    {
+        musicLevel = Mathf.Clamp(musicLevel + amount, 0, 10);
+        if (musicLevelText != null) musicLevelText.text = musicLevel.ToString();
+
+        // El valor se divide entre 10 para dar un rango de 0.0001 a 1 al Mixer
+        float volumeNormalized = Mathf.Max(musicLevel / 10f, 0.0001f);
+        gameManager.Musicvolume(LinearToLog(volumeNormalized));
+    }
+
+    private void ChangeSFXVolume(int amount)
+    {
+        sfxLevel = Mathf.Clamp(sfxLevel + amount, 0, 10);
+        if (sfxLevelText != null) sfxLevelText.text = sfxLevel.ToString();
+
+        float volumeNormalized = Mathf.Max(sfxLevel / 10f, 0.0001f);
+        gameManager.SFXVolume(LinearToLog(volumeNormalized));
+    }
+
+    private float LinearToLog(float value)
+    {
+        return Mathf.Log10(value) * 20;
     }
 
 }
